bbacf6b04f4a09f610f8718253bfb639b5dabc73
KirillDimitrievLabs <45212350+KirillDimitrievLabs@users.noreply.github.com>
2022-10-26 14:49:29 +0300

    Add project files.

---
+++ Extensions/ExtensionBlazorStrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using BlazorStrap;
+using Microsoft.AspNetCore.Components.Web;
+using static RemRelativesMaster.Pages.MyRelatives;
+
+namespace RemRelativesMaster.Extensions
+{
+    //public static class ExtensionBlazorStrap
+    //{
+    //    public static void Test(this BSTR bstr)
+    //    {
+    //        bstr.IsActive = true;
+    //    }
+    //}
+    public class BSTRWithOnHoverAnimation : BlazorStrap.BSTR
+    {
+        public bool GetIsActive(MouseEventArgs e)
+        {
+            return this.IsActive;
+        }
+    }
+}
